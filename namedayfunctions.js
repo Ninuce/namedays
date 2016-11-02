a75d7cc1fe14cc3bb1b37ff6c13153e5930c4611
Lota Plezere <lota.plezere@gmail.com>
2016-11-02 17:13:28 +0200

    Added functional search tools

--- namedayfunctions.js
+++ namedayfunctions.js
@@ -24,7 +24,7 @@ function displayDailyNamedays() {
   for (i = 0; i < dailyNameDays.length; i++) {
     dailyName += dailyNameDays[i];
     if (i < dailyNameDays.length - 1) {
-      dailyName += ", "
+      dailyName += ", ";
     }
   }
   console.log(dailyName);
@@ -32,3 +32,78 @@ function displayDailyNamedays() {
 }
 
 displayDailyNamedays();
+
+//FIND NAMES BY THE DATE
+
+// * search in JSON, return the names for the specified date
+// * display the names and date in html
+
+// * read the input
+
+function displayName() {
+  var searchDay = document.getElementById("daySelect").value;
+  var searchMonth = document.getElementById("monthSelect").value;
+  console.log(searchDay);
+  console.log(searchMonth);
+
+// * validate the input (number or not, valid date?)
+  if(searchDay <= 31 && searchMonth <= 12) {
+    console.log("validation successful, chosen date " + searchDay + "." + searchMonth);
+
+    var nameArray = nameDays[searchMonth][searchDay];
+    console.log(nameArray);
+
+    var foundNames = "";
+    for (i = 0; i < nameArray.length; i++) {
+      foundNames += nameArray[i];
+      if (i < nameArray.length - 1) {
+        foundNames += ", ";
+      }
+    }
+    console.log(foundNames);
+    alert("Namedays on " + searchDay + "." + searchMonth + " are: " + foundNames);
+  } else {
+    alert("Something went wrong, please check the dates and try again");
+  }
+}
+
+// function validateDate(input) {
+//   // var inputValidator = isNaN(input);
+//   if (input <= 31) {
+//     console.log("validator returning true");
+//     return true;
+//
+//   } else {
+//     console.log("Date above 31");
+//     return false;
+//   }
+// }
+
+
+
+
+
+// SHOW A DATE WHEN USER SEARCHES BY NAME
+
+function displayDate() {
+
+  var allMonths = Object.keys(nameDays);
+  for (i = 0; i < allMonths.length; i++) {
+  	var singleMonth = nameDays[allMonths[i]];
+
+  	var allDays = Object.keys(singleMonth);
+  	for (n = 0; n < allDays.length; n++) {
+  		var singleDay = singleMonth[allDays[n]];
+      console.log(n + ":::" + singleDay);
+
+      for(j=0; j <singleDay.length; j++) {
+        var searchName = document.getElementById("nameInput").value;
+        if(searchName === singleDay[j]) {
+          console.log("found, month: " + allMonths[i] + " day: " + allDays[n]);
+          alert(searchName + "'s nameday is on " + allDays[n] + "." + allMonths[i]);
+          return(singleDay[j]);
+        }
+      }
+    }
+  }
+}
